156ad252877f20a5d70205b8bcfe1d5710b94d61
Raphaël BERGINA <bergina.raphael@gmail.com>
2020-05-22 23:22:19 -0300

    Add code to declare oauth token

--- .circleci/scripts/zohoTokenDeploy.php
+++ .circleci/scripts/zohoTokenDeploy.php
@@ -1,5 +1,8 @@
 <?php
 
+use zcrmsdk\oauth\utility\ZohoOAuthTokens;
+use zcrmsdk\oauth\ZohoOAuth;
+
 include_once __DIR__ . '/../../vendor/autoload.php';
 
 $logDirectory = getenv('APPLICATION_LOG_FILE_PATH');
@@ -19,4 +22,11 @@ $zohoClient = new \Wabel\Zoho\CRM\ZohoClient( [
     'token_persistence_path' => $tokenDirectory
 ], getenv('ZOHO_TIMEZONE'));
 
-$zohoClient->getZohoOAuthClient()->refreshAccessToken(getenv('ZOHO_REFRESH_TOKEN'), getenv('USER_EMAIL_ID'));
+
+$zohoClient->initCLient();
+$hackGenerateToken = new ZohoOAuthTokens();
+$hackGenerateToken->setRefreshToken(getenv('ZOHO_REFRESH_TOKEN'));
+$hackGenerateToken->setAccessToken(getenv('ZOHO_ACCESS_TOKEN'));
+$hackGenerateToken->setExpiryTime(getenv('ZOHO_EXPIRE_TIME'));
+$hackGenerateToken->setUserEmailId(getenv('ZOHO_USER_EMAIL_ID'));
+ZohoOAuth::getPersistenceHandlerInstance()->saveOAuthData($hackGenerateToken);
